e7d79780b3225009247707cc6641dc015898dfe1
Seth Karim Luis Martínez <guepardo190889@gmail.com>
2020-01-10 22:45:24 -0600

    Crada vista listar para mostrar el listado de clientes
    Es posible importar datos de prueba en la base de datos en memoria H2 creando un archivo llamado import.sql
    y poniendo ahí todo lo que se desea agregar a la base de datos (como inserts) ya que Spring Boot lo busca
    por default y lo ejecuta al inicio de la aplicación

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+/* Populate tables */
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (1, 'Seth', 'Luis', 'correo@correo.com', '2020-01-10');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (2, 'Libni', 'Herrera', 'correo2@correo.com', '2020-01-10');
