2ac8e0447c916af1a633e377b98c5c05e89ca05e
Seth Karim Luis Martínez <guepardo190889@gmail.com>
2020-01-10 19:32:13 -0600

    Agregar ClienteDao (interfaz e implementación)
    @Repository - Anotación que identifica una clase como fuente de datos (se puede considerar a una entidad del patrón DAO
    como una fuente de datos)
    @PersistenceContext - Se debe colocar sobre la propiedad EntityManager y expresa la dependencia con el manejador de
    base de datos que tengamos asociado o configurado (por default, si no se configura ninguna base de datos, Spring Boot
    toma la base de datos H2, siempre y cuando se tenga la dependencia configurada en el archivo pom.xml)
    Transactional(readOnly = true): Indica que una operación se deberá realizar dentro de una transacción

---
+++ src/main/java/com/blackdeath/springboot/app/models/dao/ClienteDaoImpl.java
@@ -0,0 +1,30 @@
+package com.blackdeath.springboot.app.models.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.blackdeath.springboot.app.models.entity.Cliente;
+
+/**
+ * @author blackdeath
+ *
+ */
+@Repository
+public class ClienteDaoImpl implements IClienteDao {
+
+	@PersistenceContext
+	private EntityManager em;
+
+	@SuppressWarnings("unchecked")
+	@Transactional(readOnly = true)
+	@Override
+	public List<Cliente> findAll() {
+		return em.createQuery("from Cliente").getResultList();
+	}
+
+}
---
+++ src/main/java/com/blackdeath/springboot/app/models/dao/IClienteDao.java
@@ -0,0 +1,14 @@
+package com.blackdeath.springboot.app.models.dao;
+
+import java.util.List;
+
+import com.blackdeath.springboot.app.models.entity.Cliente;
+
+/**
+ * @author blackdeath
+ *
+ */
+public interface IClienteDao {
+	
+	public List<Cliente> findAll();
+}
