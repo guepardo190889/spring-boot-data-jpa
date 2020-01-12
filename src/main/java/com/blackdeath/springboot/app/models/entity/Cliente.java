b60e61ea19f85ff7d5da0a86639bd38a6d3f2290
Seth Karim Luis Martínez <guepardo190889@gmail.com>
2020-01-12 07:23:23 -0600

    Agregar validaciones a alta de clientes
    Anotaciones de javax.validation.constraints  para validaciones en entidad:
    @NotEmpty - Valida que un campo String no sea vacío
    @Size(min = 2, max = 64) - Valida el tamaño de un campo
    @NotNull - Valida que un objeto no sea null
    
    Para poder validar entidades, es necesario anotar la entidad con @Valid y justo al lado poner siempre
    el objeto BindingResult. Después es posible usar el método hasErrors() de BindingResult para validar
    si han habido errores en la validación
    
    Ejemplo:
    
    	@RequestMapping(value = "/form", method = RequestMethod.POST)
    	public String guardar(@Valid Cliente cliente, BindingResult result, Model model) {
    
    		if (result.hasErrors()) {
    			model.addAttribute("titulo", "Formulario de Cliente");
    			return "form";
    		}
    
    		clienteDao.save(cliente);
    
    		return "redirect:listar";
    	}

--- src/main/java/com/blackdeath/springboot/app/models/entity/Cliente.java
+++ src/main/java/com/blackdeath/springboot/app/models/entity/Cliente.java
@@ -11,6 +11,10 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -29,15 +33,21 @@ public class Cliente implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@NotEmpty
+	@Size(min = 2, max = 64)
 	private String nombre;
 
+	@NotEmpty
 	private String apellido;
 
+	@NotEmpty
+	@Email
 	private String email;
 
 	@Column(name = "create_at")
 	@Temporal(TemporalType.DATE)
 	@DateTimeFormat(pattern = "yyyy-mm-dd")
+	@NotNull
 	private Date createAt;
 
 	public Long getId() {
