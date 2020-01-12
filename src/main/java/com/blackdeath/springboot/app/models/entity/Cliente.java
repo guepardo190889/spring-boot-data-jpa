2f0ee33d84052eebc78d68fa7891ec7d3a75631c
Seth Karim Luis Martínez <guepardo190889@gmail.com>
2020-01-12 06:37:06 -0600

    Especificar formato de fecha
    Con la anotación @DateTimeFormat(pattern = "yyyy-mm-dd") es posible especificar el patrón a utilizar
    para fechas

--- src/main/java/com/blackdeath/springboot/app/models/entity/Cliente.java
+++ src/main/java/com/blackdeath/springboot/app/models/entity/Cliente.java
@@ -8,10 +8,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 /**
  * 
@@ -36,13 +37,9 @@ public class Cliente implements Serializable {
 
 	@Column(name = "create_at")
 	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "yyyy-mm-dd")
 	private Date createAt;
 
-	@PrePersist
-	public void prePersist() {
-		createAt = new Date();
-	}
-	
 	public Long getId() {
 		return id;
 	}
