1885ba23764ef408fda9a2d184a4802c8fbc4427
Seth Karim Luis Martínez <guepardo190889@gmail.com>
2020-01-13 07:10:55 -0600

    Implementación de edición de cliente

--- src/main/java/com/blackdeath/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/blackdeath/springboot/app/controllers/ClienteController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -36,7 +37,7 @@ public class ClienteController {
 	public String crear(Map<String, Object> model) {
 		Cliente cliente = new Cliente();
 		model.put("cliente", cliente);
-		model.put("titulo", "Formulario de Cliente");
+		model.put("titulo", "Alta de Cliente");
 
 		return "form";
 	}
@@ -45,12 +46,29 @@ public class ClienteController {
 	public String guardar(@Valid Cliente cliente, BindingResult result, Model model) {
 
 		if (result.hasErrors()) {
-			model.addAttribute("titulo", "Formulario de Cliente");
+			model.addAttribute("titulo", "Alta de Cliente");
 			return "form";
 		}
 
 		clienteDao.save(cliente);
 
 		return "redirect:listar";
+	}
+
+	@RequestMapping(value = "/form/{id}")
+	public String editar(@PathVariable(value = "id") Long id, Map<String, Object> model) {
+
+		Cliente cliente = null;
+
+		if (id > 0) {
+			cliente = clienteDao.findOne(id);
+		} else {
+			return "redirect:listar";
+		}
+
+		model.put("cliente", cliente);
+		model.put("titulo", "Editar Cliente");
+
+		return "form";
 	}
 }
