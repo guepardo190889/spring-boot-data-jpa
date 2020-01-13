f648eb3b6b96e4d9a72cba721f78966bf9601bff
Seth Karim Luis Martínez <guepardo190889@gmail.com>
2020-01-13 06:54:14 -0600

    Cambio a correos default

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
 /* Populate tables */
-INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (1, 'Seth', 'Luis', 'correo@correo.com', '2020-01-10');
-INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (2, 'Libni', 'Herrera', 'correo2@correo.com', '2020-01-10');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (1, 'Seth', 'Luis', 'seth.luis@correo.com', '2020-01-10');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (2, 'Libni', 'Herrera', 'libni.herrera@correo.com', '2020-01-10');
