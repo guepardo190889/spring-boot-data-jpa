4bac062f7c92f0f06931a9b063d179a0760ac07b
Seth Karim Luis Martínez <guepardo190889@gmail.com>
2020-01-14 21:01:13 -0600

    Implementación de mensajes flash
    RedirectAttributes, mediante su atributo addFlashAttribute nos permite agregar "mensajes flash", es decir,
    valores que se guardan en la sesión HTTP pero duran hasta la próxima petición y luego se borran

--- src/main/java/com/blackdeath/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/blackdeath/springboot/app/controllers/ClienteController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.blackdeath.springboot.app.models.entity.Cliente;
 import com.blackdeath.springboot.app.models.service.IClienteService;
@@ -46,27 +47,38 @@ public class ClienteController {
 	}
 
 	@RequestMapping(value = "/form", method = RequestMethod.POST)
-	public String guardar(@Valid Cliente cliente, BindingResult result, Model model, SessionStatus status) {
+	public String guardar(@Valid Cliente cliente, BindingResult result, Model model, SessionStatus status,
+			RedirectAttributes flash) {
 
 		if (result.hasErrors()) {
 			model.addAttribute("titulo", "Alta de Cliente");
 			return "form";
 		}
 
+		String mensajeFlash = (cliente.getId() != null) ? "Cliente editado con éxito" : "Cliente guardado con éxito";
+
 		clienteService.save(cliente);
 		status.setComplete();
+
+		flash.addFlashAttribute("success", mensajeFlash);
 
 		return "redirect:/listar";
 	}
 
 	@RequestMapping(value = "/form/{id}")
-	public String editar(@PathVariable(value = "id") Long id, Map<String, Object> model) {
+	public String editar(@PathVariable(value = "id") Long id, Map<String, Object> model, RedirectAttributes flash) {
 
 		Cliente cliente = null;
 
 		if (id > 0) {
 			cliente = clienteService.findOne(id);
+
+			if (cliente == null) {
+				flash.addFlashAttribute("error", "El Id del Cliente no existe");
+				return "redirect:/listar";
+			}
 		} else {
+			flash.addFlashAttribute("error", "El Id del Cliente no puede ser cero");
 			return "redirect:/listar";
 		}
 
@@ -77,9 +89,10 @@ public class ClienteController {
 	}
 
 	@RequestMapping(value = "/eliminar/{id}")
-	public String eliminar(@PathVariable(value = "id") Long id) {
+	public String eliminar(@PathVariable(value = "id") Long id, RedirectAttributes flash) {
 		if (id > 0) {
 			clienteService.delete(id);
+			flash.addFlashAttribute("success", "Cliente eliminado con éxito");
 		}
 
 		return "redirect:/listar";
