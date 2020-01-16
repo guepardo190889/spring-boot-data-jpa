7a6dc4c3eccc367ff9134e93e21cc2ca90bdac10
Seth Karim Luis Martínez <guepardo190889@gmail.com>
2020-01-16 07:47:34 -0600

    Configuraciones para subida de archivos
    Se agrega campo foto a la entidad Cliente
    Se agrega enctype al formulario form.html -> Existen 2 tipos de enctype, cuyas diferencias se explican acá:
    https://javarevisited.blogspot.com/2017/06/difference-between-applicationx-www-form-urlencoded-vs-multipart-form-data.html
    Se agregan configuraciones a application.properties para limitar el tamaño de subida de archivos

--- src/main/java/com/blackdeath/springboot/app/models/entity/Cliente.java
+++ src/main/java/com/blackdeath/springboot/app/models/entity/Cliente.java
@@ -48,6 +48,8 @@ public class Cliente implements Serializable {
 	@NotNull
 	private Date createAt;
 
+	private String foto;
+
 	public Long getId() {
 		return id;
 	}
@@ -86,6 +88,14 @@ public class Cliente implements Serializable {
 
 	public void setCreateAt(Date createAt) {
 		this.createAt = createAt;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
 	}
 
 }
