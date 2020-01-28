014778b8ad5a3c6c96d5d21cb43eed08cfc191d4
Seth Karim Luis Martínez <guepardo190889@gmail.com>
2020-01-28 07:53:18 -0600

    Se agrega entidad Factura y se crea relación con Cliente usando anotaciones
    Anotación @OneToMany: @OneToMany(mappedBy = "cliente", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
    El "One" hace referencia a la clase actual y el "ToMany" hace referencia al campo sobre el que se agrega la anotación.
    El fetch "LAZY" evita que se carguen entidades propiedad de una entidad al consultar a la entidad contenedora; en el
    ejemplo, se evita que se carguen todas las facturas del cliente cuando se consulta el cliente ya que se realizamos
    la consulta de todos los clientes, de no ser el fetch LAZY sino EAGER, se estarían cargando todas las facturas
    de todos los clientes y ni si quiera estaríamos mostrando las facturas en la consulta de clientes. Con Lazy, las facturas
    solo se van a cargar cuando se invoque el método getFacturas de la entidad Cliente.
    El cascade "ALL" indica que si se elimina o guarda la entidad "padre" (cliente, por ejemplo), las "hijas" (facturas, por
    ejemplo), también deben ser guardadas o eliminadas "en cascada".

--- src/main/java/com/blackdeath/springboot/app/models/entity/Cliente.java
+++ src/main/java/com/blackdeath/springboot/app/models/entity/Cliente.java
@@ -1,13 +1,18 @@
 package com.blackdeath.springboot.app.models.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -50,8 +55,19 @@ public class Cliente implements Serializable {
 
 	private String foto;
 
+	@OneToMany(mappedBy = "cliente", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	List<Factura> facturas;
+
+	public Cliente() {
+		this.facturas = new ArrayList<>();
+	}
+
 	public String getNombreConApellido() {
 		return getNombre() + " " + getApellido();
+	}
+
+	public void addFactura(Factura factura) {
+		this.facturas.add(factura);
 	}
 
 	public Long getId() {
@@ -100,6 +116,14 @@ public class Cliente implements Serializable {
 
 	public void setFoto(String foto) {
 		this.foto = foto;
+	}
+
+	public List<Factura> getFacturas() {
+		return facturas;
+	}
+
+	public void setFacturas(List<Factura> facturas) {
+		this.facturas = facturas;
 	}
 
 }
